863bc48b06bab0b8a8b156a9f11b8bc932712e14
Davi Freire <fsdavi.dev@gmail.com>
2024-07-08 16:59:41 -0300

    Implementing EnvConfig interface

--- src/shared/infrastructure/env-config/env-config.module.ts
+++ src/shared/infrastructure/env-config/env-config.module.ts
@@ -1,7 +1,7 @@
-import { Module } from '@nestjs/common';
-import { EnvConfigService } from './env-config.service';
+import { Module } from '@nestjs/common'
+import { EnvConfigService } from './env-config.service'
 
 @Module({
-  providers: [EnvConfigService]
+  providers: [EnvConfigService],
 })
 export class EnvConfigModule {}
